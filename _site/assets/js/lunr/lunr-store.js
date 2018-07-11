edf98c0a44b9c6cd70501119dd7ddddf75afd881
kevin11h <kevhung11@gmail.com>
2018-07-11 15:52:20 -0700

    push to master branch

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Storybook Wish List",
-        "excerpt":"I love books, because once upon a time a young boy finds, some special storybook who spoke to him first, \"I love you\" Miracles = Meaning + Work When one senses why and how, heart and mind, spirituality and business, meaning and work, miracles occur. Miracles happen in everyday life...","categories": [],
+        "excerpt":"I love books, because once upon a time a young boy finds, some special storybook who spoke to him first, \"I love you\" Miracles = Meaning + Work When one senses why and how, heart and mind, spirituality and business, meaning and work, miracles occur. Miracles happen in life everyday...","categories": [],
         "tags": ["book","list"],
         "url": "http://localhost:4000/book-wishlist/",
         "teaser":null}]
